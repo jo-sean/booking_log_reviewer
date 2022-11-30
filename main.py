f02d557a5dd85503a223073e15500d56f93d73b4
Sean <joseanmanuel@gmail.com>
2022-12-01 10:54:36 +1300

    Consolidated code from previous project.

---
+++ main.py
@@ -0,0 +1,13 @@
+# Sean Perez
+# Collab: Dominic Stewart
+# Date: 01/12/2022
+
+from read_excel import read_excel_file
+
+
+def main():
+    read_excel_file()
+
+
+if __name__ == "__main__":
+    main()
