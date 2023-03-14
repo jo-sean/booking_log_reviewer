5cd07026f060c44418aa2cd0e7bd0cf40e850ae4
domistu <dstuart950+github@gmail.com>
2023-03-14 15:18:42 +1300

    fixed issue with dates not being included

--- read_excel.py
+++ read_excel.py
@@ -19,34 +19,6 @@ def read_excel_file():
 
     # Retrieves only the file name from the path
     file_name = os.path.splitext(os.path.basename(file_name))[0]
-
-    # Get the dates included in the security report
-    keylist = list(df.keys())
-    dateRange = df[keylist[0]].loc[df[keylist[0]][1].str.contains('From', case=False) == True][1].iat[0]
-    dateRange = dateRange.split(' ')
-
-    dateRange[1] = dateRange[1].split('/')
-    for i in range(len(dateRange[1])):
-        if len(dateRange[1][i]) < 2:
-            dateRange[1][i] = f'0{dateRange[1][i]}'
-    dateRange[1] = f'{dateRange[1][0]}-{dateRange[1][1]}-{dateRange[1][2]}'
-
-    dateRange[2] = dateRange[2].split(':')
-    for i in range(len(dateRange[2])):
-        if len(dateRange[2][i]) < 2:
-            dateRange[2][i] = f'0{dateRange[2][i]}'
-    dateRange[2] = f'{dateRange[2][0]}:{dateRange[2][1]}:{dateRange[2][2]}'
-    
-    if 'a' or 'A' in dateRange[3]:
-        dateRange[3] = 'AM'
-    else:
-        dateRange[3] = 'PM'    
-
-    startDate = pd.to_datetime(f"{dateRange[1]} {dateRange[2]} {dateRange[3]}", dayfirst=True)
-
-    for i in range(8):
-        config.dateList.append(startDate)
-        startDate = startDate + pd.DateOffset(days=1)
         
     for key, value in df.items():
         df_col_2 = value[value[2].str.contains('Colliers|Linwood', case=False) == True]
@@ -57,7 +29,9 @@ def read_excel_file():
     # Concatenate dataframes in dictionary into a single dataframe
     df = pd.concat(df)
 
-    filtered_df = df.loc[df[col_index].str.contains('open by|close by', case=False) == True]
+    filtered_df.loc[filtered_df['Room'] == "administration", 'Room'] = "10"
+
+    filtered_df = filtered_df.loc[filtered_df[col_index].str.contains('open by|close by', case=False) == True]
     filtered_df = filtered_df.dropna(axis=1)
     # Keep only rooms from list in dataframe
     filtered_df = filtered_df[filtered_df['Room'].isin(rooms_for_bookings)]
@@ -65,6 +39,12 @@ def read_excel_file():
     filtered_df = filtered_df[filtered_df[col_index].str.contains("65522")==False]
     # Remove PCCCT from dataframe
     filtered_df = filtered_df[filtered_df[col_index].str.contains("PCCCT")==False]
+
+    filtered_df[1] = filtered_df[1].apply(stringToTimestamp)
+
+    config.dateList = filtered_df[1].map(pd.Timestamp.date).unique()
+    config.dateList.sort()
+
     config.securityDF= filtered_df
 
 def read_excel_files():
@@ -88,34 +68,6 @@ def read_excel_files():
             df_dict.append(df)
 
     for df in df_dict:
-        # Get the dates included in the security report
-        keylist = list(df.keys())
-        dateRange = df[keylist[0]].loc[df[keylist[0]][1].str.contains('From', case=False) == True][1].iat[0]
-        dateRange = dateRange.split(' ')
-
-        dateRange[1] = dateRange[1].split('/')
-        for i in range(len(dateRange[1])):
-            if len(dateRange[1][i]) < 2:
-                dateRange[1][i] = f'0{dateRange[1][i]}'
-        dateRange[1] = f'{dateRange[1][0]}-{dateRange[1][1]}-{dateRange[1][2]}'
-
-        dateRange[2] = dateRange[2].split(':')
-        for i in range(len(dateRange[2])):
-            if len(dateRange[2][i]) < 2:
-                dateRange[2][i] = f'0{dateRange[2][i]}'
-        dateRange[2] = f'{dateRange[2][0]}:{dateRange[2][1]}:{dateRange[2][2]}'
-        
-        if 'a' or 'A' in dateRange[3]:
-            dateRange[3] = 'AM'
-        else:
-            dateRange[3] = 'PM'    
-
-        startDate = pd.to_datetime(f"{dateRange[1]} {dateRange[2]} {dateRange[3]}", dayfirst=True)
-
-        for i in range(7):
-            config.dateList.append(startDate)
-            startDate = startDate + pd.DateOffset(days=1)
-
         for key, value in df.items():
             df_col_2 = value[value[2].str.contains('Colliers|Linwood', case=False) == True]
             df_col_2 = df_col_2[2].str.split(' - ').str[-1].str.lower()
@@ -124,8 +76,6 @@ def read_excel_files():
 
         df = pd.concat(df)
         df_list.append(df)
-        
-    config.dateList.sort()
 
     filtered_df = pd.concat(df_list)
 
@@ -140,4 +90,31 @@ def read_excel_files():
     # Remove PCCCT from dataframe
     filtered_df = filtered_df[filtered_df[col_index].str.contains("PCCCT")==False]
 
+    filtered_df[1] = filtered_df[1].apply(stringToTimestamp)
+
+    config.dateList = filtered_df[1].map(pd.Timestamp.date).unique()
+    config.dateList.sort()
+
     config.securityDF= filtered_df
+
+def stringToTimestamp(string):
+    dateRange = string.split(' ')        
+
+    dateRange[0] = dateRange[0].split('/')
+    for i in range(len(dateRange[0])):
+        if len(dateRange[0][i]) < 2:
+            dateRange[0][i] = f'0{dateRange[0][i]}'
+    dateRange[0] = f'{dateRange[0][0]}-{dateRange[0][1]}-{dateRange[0][2]}'
+
+    dateRange[2] = dateRange[2].split(':')
+    for i in range(len(dateRange[2])):
+        if len(dateRange[2][i]) < 2:
+            dateRange[2][i] = f'0{dateRange[2][i]}'
+    dateRange[2] = f'{dateRange[2][0]}:{dateRange[2][1]}:{dateRange[2][2]}'
+
+    if 'a' in dateRange[3] or 'A' in dateRange[3]:
+        dateRange[3] = 'AM'
+    else:
+        dateRange[3] = 'PM' 
+
+    return pd.to_datetime(f'{dateRange[0]} {dateRange[2]} {dateRange[3]}', format='%d-%m-%Y %I:%M:%S %p')
